a9fe89d3dc7bea12fe4faaec8bb5ab027b1fb28c
Eric Wood <ericchristopherphotos@gmail.com>
2023-12-23 22:34:44 -0500

    Ubuntu 20.04 updates

--- README.md
+++ README.md
@@ -3,11 +3,11 @@ This set of instructions is a modified version from https://gist.github.com/zach
 
 To setup phenix, follow the steps below:
 
-1) Setup a machine with Ubuntu 18.04 LTS
+1) Setup a machine with Ubuntu 20.04 LTS
 2) Enable virtualization in the BIOS
 3) If using a VM, make sure nested VT-x/AMD-V is checked
 4) If using a VM, allocate at least 4 GB RAM.  Otherwise, 3 GB of free RAM should be sufficient.
-5) Navigate to the home directory on the Ubuntu 18.04 LTS (e.g. `cd ~`)
+5) Navigate to the home directory on the Ubuntu 20.04 LTS (e.g. `cd ~`)
 6) `sudo apt -y install git`
 7) Clone this repository `git clone https://github.com/eric-c-wood/phenix-setup.git`
 8) `cd phenix-setup`
--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #Prereq  
-#ubuntu 16-18
+#ubuntu 20.04
 # Ensure virtulization is enabled in bios
 #if using a VM ensure VT-x/AMD-V is enabled for VM
 # At least 3 GB of free RAM, so approx 4 GB with 1 GB for OS and 4 cores
@@ -8,7 +8,7 @@
 #Packages:
 echo "Getting base set of packages"
 sudo apt update
-sudo apt -y install libpcap-dev libreadline-dev qemu qemu-kvm openvswitch-switch build-essential unzip genisoimage debootstrap syslinux extlinux bird ntpdate curl tmux net-tools bison
+sudo DEBIAN_FRONTEND=noninteractive apt -y install git libpcap-dev libreadline-dev qemu qemu-kvm openvswitch-switch build-essential unzip genisoimage debootstrap syslinux extlinux bird ntpdate curl tmux net-tools bison
 
 #Git some helper tools ((not required but makes dealing with multiple go versions easier) 
 echo "Installing GVM"
@@ -18,26 +18,28 @@ gvm install go1.4 -B
 gvm use go1.4
 export GOROOT_BOOTSTRAP=$GOROOT
 
-#go 1.12 to build minimega
-echo "Installing go 1.14"
-gvm install go1.14
+#go 1.17.13 to build compile go 1.20.7
+echo "Installing go 1.17.13"
+gvm install go1.17.13
+gvm use go1.17.13
+export GOROOT_BOOTSTRAP=$GOROOT
 
-#go 1.17 to build phenix 
-echo "Installing go 1.18"
-gvm install go1.18
+#go 1.20.7 to build phenix 
+echo "Installing go 1.20.7"
+gvm install go1.20.7
 
 #Phenix web server dependencies
 
 # NVM Manager ( not required but makes dealing with npm easier)
 echo "Installing NVM"
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.39.7/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 # Install node js and npm
 echo "Installing node js and npm"
-nvm install 14.2
+nvm install 14.21.3
 
 # Install yarn
 echo "Installing yarn"
@@ -48,10 +50,12 @@ sudo apt install -y --no-install-recommends yarn
 
 # Install google proto buffers
 echo "Installing Google proto buffers"
-PROTOC_ZIP=protoc-3.7.1-linux-x86_64.zip
-curl -OL https://github.com/protocolbuffers/protobuf/releases/download/v3.7.1/$PROTOC_ZIP
+PROTOC_ZIP=protoc-3.14.0-linux-x86_64.zip
+curl -OL https://github.com/protocolbuffers/protobuf/releases/download/v3.14.0/$PROTOC_ZIP
 sudo unzip -o $PROTOC_ZIP -d /usr/local bin/protoc
 sudo unzip -o $PROTOC_ZIP -d /usr/local 'include/*'
+sudo chmod 755 /usr/local/bin/protoc
+sudo chmod -R 755 /usr/local/include/google
 rm -f $PROTOC_ZIP
 
 #setup image directory
@@ -67,18 +71,18 @@ sudo chown -R $USER:$USER /opt/minimega
 cp ~/phenix-setup/mini*.service /opt/minimega
 sed -i s/MM_CONTEXT=minimega/MM_CONTEXT=$(hostname -s)/ /opt/minimega/minimega.service
 cd /opt/minimega/scripts;
-gvm use go1.14;
+gvm use go1.20.7;
 ./build.bash
 
 #setup phenix
 echo "Setting up phenix"
 cd ~
-git clone https://github.com/sandia-minimega/phenix.git
-sudo mv ~/phenix /opt/phenix
+git clone https://github.com/sandialabs/sceptre-phenix.git
+sudo mv ~/sceptre-phenix /opt/phenix
 sudo chown -R $USER:$USER /opt/phenix
 cp ~/phenix-setup/phenix*.service /opt/phenix
 cd /opt/phenix;
-gvm use go1.18;
+gvm use go1.20.7;
 make bin/phenix
 
 #setup ovs bridge
